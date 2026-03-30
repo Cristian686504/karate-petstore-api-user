eb90791931e6057aa22e7692c9cec8fd113a8607
Cristian686504 <cristian.renz@estudiantes.utec.edu.uy>
2026-03-30 14:13:03 -0300

    feat(user): add UserRunner for domain execution

---
+++ src/test/java/template/user/UserRunner.java
@@ -0,0 +1,11 @@
+package template.user;
+
+import com.intuit.karate.junit5.Karate;
+
+class UserRunner {
+
+    @Karate.Test
+    Karate testUser() {
+        return Karate.run("user-crud.feature").relativeTo(getClass());
+    }
+}
